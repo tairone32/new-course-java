68397bd35435d169932654dcbe738e6c4fe03b2d
Caio Tairone <caio.vieira@ab-inbev.com>
2023-08-24 08:47:44 -0300

    Change name project

---
+++ alturas/src/entities/People.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class People {
+
+    private String name;
+    private double height;
+
+    public People(String name, double height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
